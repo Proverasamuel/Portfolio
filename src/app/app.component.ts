761978612313d67b68d87b3208ef41bff79e4475
Provera Samuel <proverasamuel2022@gmail.com>
2025-03-01 17:11:20 +0100

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./components/hero/hero.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SobreComponent } from "./components/sobre/sobre.component";
+import { ProjetosComponent } from "./components/projetos/projetos.component";
+import { ContatoComponent } from "./components/contato/contato.component";
+import { HabilidadesComponent } from "./components/habilidades/habilidades.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeroComponent, NavbarComponent, SobreComponent, ProjetosComponent, ContatoComponent, HabilidadesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Permite alternância de tema
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
