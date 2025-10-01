2086790b073543e7c38dd6d44765a66c78059b36
Provera Samuel <proverasamuel2022@gmail.com>
2025-10-01 22:53:38 +0100

    Add emailjs integration for contact form submission

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -1,17 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css'
 })
 export class ContatoComponent {
- /** Tratamento do envio do formulário de contato */
-  onSubmitContactForm(form: any): void {
-    // Lógica futura com EmailJS aqui
-    alert('Obrigado pela mensagem! Entrarei em contato em breve.');
-    form.reset();
+
+  loading = false;
+   sendEmail(form: any) {
+    if (!form.valid) return;
+     this.loading = true;
+    emailjs.send(
+      'service_boxj2kd',
+      'template_8il9zkn',
+      {
+        name: form.value.name,
+        email: form.value.email,
+        subject: form.value.subject,
+        message: form.value.message
+      },
+      'WWRlEUSZH-eSS6v-B'
+    )
+    .then((response: EmailJSResponseStatus) => {
+      this.loading = false;
+      console.log('Email enviado com sucesso!', response.status, response.text);
+      Swal.fire({
+        title: 'Mensagem enviada!',
+        text: 'Obrigado por entrar em contacto. Vou responder o mais rápido possível!',
+        icon: 'success',
+        confirmButtonColor: '#009CD0' // tua cor do Tailwind primary/accent
+      });
+
+      form.reset();
+    }, (err) => {
+      this.loading = false;
+      console.error('Erro ao enviar email:', err);
+      Swal.fire({
+        title: '❌ Erro ao enviar!',
+        text: 'Ocorreu um problema. Tente novamente mais tarde.',
+        icon: 'error',
+        confirmButtonColor: '#DC2626'
+      });
+    });
   }
+
 }
